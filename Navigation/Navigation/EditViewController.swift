a11e9a436852ca6f2509a5d4be5317795755ef4e
sue0725 <suhyunn0725@gmail.com>
2021-08-22 23:58:11 +0900

    update

--- Navigation/Navigation/EditViewController.swift
+++ Navigation/Navigation/EditViewController.swift
@@ -7,15 +7,48 @@
 
 import UIKit
 
-class EditViewController: UIViewController {
+protocol EditDelegate {
+    func didMessageEditDone(_ controller: EditViewController, message: String)
+    func didImageOnOffDone(_ controller: EditViewController, isOn: Bool)
+}
 
+class EditViewController: UIViewController {
+    
+    
+    
+    var textWayValue: String = ""
+    var textMessage: String = ""
+    var delegate : EditDelegate?
+    var isOn = false
+
+    @IBOutlet var lblWay: UILabel!
+    @IBOutlet var txMessage: UITextField!
+    @IBOutlet var swIsOn: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        lblWay.text = textWayValue
+        txMessage.text = textMessage
+        swIsOn.isOn = isOn
     }
     
 
+    @IBAction func btnDone(_ sender: UIButton) {
+        if delegate != nil {
+            delegate?.didMessageEditDone(self, message: txMessage.text!)
+            delegate?.didImageOnOffDone(self, isOn: isOn)
+        }
+        _ = navigationController?.popViewController(animated: true)
+    }
+    @IBAction func swImageOnOff(_ sender: UISwitch) {
+        if sender.isOn {
+            isOn = true
+        } else {
+            isOn = false
+        }
+    }
     /*
     // MARK: - Navigation
 
--- Navigation/Navigation/ViewController.swift
+++ Navigation/Navigation/ViewController.swift
@@ -7,13 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, EditDelegate {
+    let imgOn = UIImage(named: "lamp_on.png")
+    let imgOff = UIImage(named: "lamp_off.png")
+    
+    var isOn = true
 
+    @IBOutlet var txMessage: UITextField!
+    @IBOutlet var imgView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        imgView.image = imgOn
     }
 
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let editViewController = segue.destination as! EditViewController
+        if segue.identifier == "editBarButton"{
+            editViewController.textWayValue = "segue : use button"
+        } else if segue.identifier == "editBarButton" {
+            editViewController.textWayValue = "segue : use Bar Button"
+        }
+        editViewController.textMessage = txMessage.text!
+        editViewController.isOn = isOn
+        editViewController.delegate = self
+    }
+    func didMessageEditDone(_ controller: EditViewController, message: String) {
+        txMessage.text = message
+    }
+    func didImageOnOffDone(_ controller: EditViewController, isOn: Bool){
+        if isOn {
+            imgView.image = imgOn
+            self.isOn = true
+        } else {
+            imgView.image = imgOff
+            self.isOn = false
+        }
+    }
 }
 
